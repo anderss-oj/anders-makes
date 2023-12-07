a2585529f0c5d5bccadb76ee0437fe6f9a0a39c9
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2023-12-07 17:30:39 -0500

    changed vite config to add the second page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { resolve } from 'path'
+
 export default {
     // config options
     base : '/',
     build: {
+      rollupOptions : {
+        input: {
+          main : resolve(__dirname, 'index.html'), 
+          page1 : resolve(__dirname, 'pages/page1.html')
+        }
+      }, 
       outDir: 'dist',
     },
   };
