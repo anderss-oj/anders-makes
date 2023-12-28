18d8db0786c1e4809d1b37daa9cbfe11812dbeb7
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2023-12-28 13:57:36 -0500

    added the other pages to vite.config.js rollup

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default {
       rollupOptions : {
         input: {
           main : resolve(__dirname, 'index.html'), 
-          page1 : resolve(__dirname, 'pages/page1.html')
+          page1 : resolve(__dirname, 'pages/page1.html'),
+          page2 : resolve(__dirname, 'pages/page2.html'),
+          page3 : resolve(__dirname, 'pages/page3.html'),
+          page4 : resolve(__dirname, 'pages/page4.html')
         }
       }, 
       outDir: 'dist',
