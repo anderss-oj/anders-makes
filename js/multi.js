0eedbc31e70be50fc917e99447d4525dd9aa9a55
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-02-23 11:29:20 -0500

    little push

--- js/multi.js
+++ js/multi.js
@@ -1,16 +1,28 @@
 // Input functionality
+const pageContainer = document.getElementById('page');
+
 document.addEventListener("DOMContentLoaded", function() {
     const animationInput = document.getElementById('animationInput');
     animationInput.addEventListener('input', (event) => {
         const inputText = event.target.value.trim().toLowerCase();
-        if (inputText === 'slow') {
-            const elements = document.querySelectorAll('.slow');
-            elements.forEach(element => {
-                element.dataset.endAfterNextIteration = 'true';
-            });
-            // Clear the input after the user inputs 'slow'
-            animationInput.value = '';
-        }
+        animationInput.addEventListener("keyup", function(event) {
+            if (event.key === "Enter") { 
+            // enter key event listener 
+                if (inputText === 'slow') {
+                    const elements = document.querySelectorAll('.slow');
+                    elements.forEach(element => {
+                        element.dataset.endAfterNextIteration = 'true';
+                    });
+                    // Clear the input after the user inputs 'slow'
+                    animationInput.value = '';
+
+                    // now we add new elements
+                    const part1Element1 = document.createElement('div');
+                    part1Element1.className = 'part1 1'
+                    // part1Element1. //adding elements?
+                }
+            }
+        })
     });
 
     // Function to remove elements after animation completes
