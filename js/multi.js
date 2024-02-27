a9053f19c744ff7521171f8a763449a84722adaa
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-02-27 16:17:08 -0500

    okay we trying to get somewhere

--- js/multi.js
+++ js/multi.js
@@ -1,25 +1,34 @@
-// Input functionality
-const pageContainer = document.getElementById('page');
+// var declaration
+
+const page = document.getElementById('page');
+const slowElements = document.querySelectorAll('.slow');
+const animationInput = document.getElementById('animationInput');
+// const mediumPage = $('#mediumPage');
 
 document.addEventListener("DOMContentLoaded", function() {
-    const animationInput = document.getElementById('animationInput');
     animationInput.addEventListener('input', (event) => {
         const inputText = event.target.value.trim().toLowerCase();
         animationInput.addEventListener("keyup", function(event) {
             if (event.key === "Enter") { 
             // enter key event listener 
+
+            // this is what happens when we input slow. we take away the intro, and add the medium page
+
                 if (inputText === 'slow') {
-                    const elements = document.querySelectorAll('.slow');
-                    elements.forEach(element => {
+                    slowElements.forEach(element => {
                         element.dataset.endAfterNextIteration = 'true';
                     });
                     // Clear the input after the user inputs 'slow'
                     animationInput.value = '';
-
-                    // now we add new elements
-                    const part1Element1 = document.createElement('div');
-                    part1Element1.className = 'part1 1'
-                    // part1Element1. //adding elements?
+                    // make sure the elements are clear before adding                    
+                    if (page.innerHTML === '') {
+                        console.log ('slow is gone');
+                        // now we add new elements
+                        addMediumElements();
+                    }
+                    else {
+                        console.log('pageDiv check did not work');
+                    }
                 }
             }
         })
@@ -42,6 +51,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Call the function for each slow animation
-    removeElementsAfterAnimation('.slow');
+    removeElementsAfterAnimation('.slow', );
 
+    function addMediumElements() {
+        $('#page').append("<div id='mediumRandomCont'>medium medium medium medium</div>")
+        const mediumRandomCont = $('#mediumRandomCont');
+        $(mediumRandomCont).addClass('showing');
+    }
 });
