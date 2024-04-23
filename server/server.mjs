60cd742dc780bcbc838128f9b7e7a4cd3b77b1db
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-04-23 14:16:05 -0400

    exporting server? changing /clicks in clickTracker.js

--- server/server.mjs
+++ server/server.mjs
@@ -40,7 +40,8 @@ app.post('/clicks', (req, res) => {
     });
 });
 
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+export default server; // Export the server instance
