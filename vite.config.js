7c1aedd267d260b9692660682c6dc5d50ac334d5
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-04-25 13:40:46 -0400

    okay some prog

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default {
         directory: resolve(__dirname, 'pages/work/directory.html'),
         multi: resolve(__dirname, 'pages/multi.html'),
         slowing: resolve(__dirname, 'slowing3.html'),
-
         manNmachine: resolve(__dirname, 'pages/work/machineNman.html'),
         bitRot: resolve(__dirname, 'pages/work/bitRot.html'),
         terms: resolve(__dirname, 'pages/work/terms.html'),
