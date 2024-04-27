b9a0be9eb2aed8510247b78cb71920a5833635b9
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-04-27 13:15:28 -0400

    init filing cabinet

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default {
         directory: resolve(__dirname, 'pages/work/directory.html'),
         multi: resolve(__dirname, 'pages/multi.html'),
         slowing: resolve(__dirname, 'slowing.html'),
+        files: resolve(__dirname, 'files.html'),
         manNmachine: resolve(__dirname, 'pages/work/machineNman.html'),
         bitRot: resolve(__dirname, 'pages/work/bitRot.html'),
         terms: resolve(__dirname, 'pages/work/terms.html'),
