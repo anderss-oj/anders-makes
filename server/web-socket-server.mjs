7a1a5a2841ae99c3c4e746a8fcaa84bbfce4436f
anderss-oj <123219287+anderss-oj@users.noreply.github.com>
2024-05-03 12:57:20 -0400

    saved changes

--- server/web-socket-server.mjs
+++ server/web-socket-server.mjs
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001; // Choose a different port for the WebSoc
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:4173', 'http://anders-makes.com'],  // Update the origin to match your client
+    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://anders-makes.com'],  // Update the origin to match your client
     methods: ['GET', 'POST']
   }
 });
